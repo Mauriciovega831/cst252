8203b228402ec52f9f3e074b33ac7bec2f5fa987
Mauriciovega831 <70286178+Mauriciovega831@users.noreply.github.com>
2021-04-06 15:40:14 -0700

    lab16

---
+++ lab16/js/lab.js
@@ -0,0 +1,35 @@
+// Author: Mauricio Vega
+// Date: April 6, 2021
+// Licensen: Public
+
+
+//add button with alert
+
+$("#output").append("<button id=my-button>Click Me For Surpise 1");
+$("#my-button").click(function (){
+  alert("I did it :,). I am so Proud!</3");
+})
+
+//style alert button
+$("#my-button").css("background-color", "#33691e");
+$("#my-button").css("color", "snow");
+
+//changes button 3 from green to black
+$("#output").append("<button id=button-two>Click Me For Surprise 2");
+$("#button-two").css("background-color", "#33691e");
+$("#button-two").css("color","snow");
+$("#button-two").click(function(){
+  $("#button-three").css("background-color","red");
+})
+
+//changes button 2 from green to white
+$("#output").append("<button id=button-three>Click Me For Surprise 3");
+$("#button-three").css("background-color","#3361e");
+$("#button-three").css("color","snow");
+$("#button-three").click(function (){
+  $("#button-two").css("background-color","cornflowerBlue");
+})
+
+//style buttons
+$("button").css ("padding","5px");
+$("button").css ("margin","5px");
